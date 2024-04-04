2cbcceae70c88cd0d22b2d8595b82f4e997524f1
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-04 08:13:33 +0545

    slider admin

--- slider/admin.py
+++ slider/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Slider)
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'image']
+    search_fields = ['title', 'url']
