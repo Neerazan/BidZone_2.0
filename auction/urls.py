7f0925606ab60830c0d28a0b46ba6234b46f2c21
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-04 23:03:21 +0545

    Collection CRUD

--- auction/urls.py
+++ auction/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import CollectionView
+from .views import CollectionView, CollectionDetailsView
 
 urlpatterns = [
-    path('collection/', CollectionView.as_view(), name='collection-view')
+    path('collection/', CollectionView.as_view()),
+    path('collection/<int:pk>/', CollectionDetailsView.as_view())
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,12 +1,37 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Collection
 from .serializers import CollectionSerializers
 
 class CollectionView(APIView):
     def get(self, request):
-        queryset = Collection.objects.all()
+        queryset = Collection.objects.order_by('pk').all()
         serializer = CollectionSerializers(queryset, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CollectionSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class CollectionDetailsView(APIView):
+    def get(self, request, pk):
+        queryset = get_object_or_404(Collection, pk=pk)
+        serializer = CollectionSerializers(queryset)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        serializer = CollectionSerializers(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
