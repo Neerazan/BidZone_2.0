6d3ace53a8847a88870b57f5699f5a9b4d6b0fa5
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-06 15:24:32 +0545

    Getting current user profile

--- auction/serializers.py
+++ auction/serializers.py
@@ -30,7 +30,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class CustomerSerializer(serializers.ModelSerializer):
 
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'first_name', 'last_name', 'phone', 'birth_date', 'membership']
--- auction/views.py
+++ auction/views.py
@@ -2,6 +2,8 @@ from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
@@ -52,6 +54,18 @@ class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, Ge
     queryset = Customer.objects.select_related('user').all()
     serializer_class = CustomerSerializer
 
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request):
+        customer = Customer.objects.get(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
 class WishlistViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet, DestroyModelMixin):
 
