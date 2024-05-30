1a5e8edb353fe519f4f6e442368fbee2e1d1c107
Neerajan <nirajandhakal634@gmail.com>
2024-05-30 08:27:08 +0545

    Reset password template added

--- auction/email.py
+++ auction/email.py
@@ -8,3 +8,12 @@ class ActivationEmail(email.ActivationEmail):
         context = super().get_context_data()
         context['frontend_url'] = f"http://localhost:5173/activate-user/{context['uid']}/{context['token']}"
         return context
+
+
+class PasswordResetEmail(email.PasswordResetEmail):
+    template_name = 'account/password_reset.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['frontend_url'] = f"http://localhost:5173/reset-password/{context['uid']}/{context['token']}"
+        return context
