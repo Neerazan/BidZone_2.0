84ba3653ab3703792d9489f65f2d443411863f4b
Neerajan <nirajandhakal634@gmail.com>
2024-05-31 08:48:57 +0545

    bug fixed

--- auction/tasks.py
+++ auction/tasks.py
@@ -1,46 +1,54 @@
-
 from celery import shared_task
 from django.utils import timezone
 from datetime import timedelta
 from .models import Auction, Bid, Delivery, UserCoin, Customer, Transaction
 from django.db import transaction
 
-
 @shared_task
 def process_completed_auctions():
     now = timezone.localtime().replace(second=0, microsecond=0)
     print(f"[DATE :::: TIME] {now}")
-    completed_auctions = Auction.objects.filter(ending_time=now, auction_status=Auction.AUCTION_ACTIVE)
-    print(f"completed auctions: {completed_auctions}")
 
-    with transaction.atomic():
-        if completed_auctions:
-            for auction in completed_auctions:
-                heighest_bid = Bid.objects.filter(auction=auction).order_by('-amount').first()
+    # Use a small time range to account for any slight differences in ending_time
+    time_range_start = now - timedelta(minutes=1)
+    time_range_end = now + timedelta(minutes=1)
+    
+    completed_auctions = Auction.objects.filter(
+        ending_time__range=(time_range_start, time_range_end),
+        auction_status=Auction.AUCTION_ACTIVE
+    )
+    print(f"Completed auctions: {completed_auctions}")
 
-                if heighest_bid:
-                    auction.auction_status = auction.AUCTION_COMPLETED
-                    auction.save()
+    for auction in completed_auctions:
+        with transaction.atomic():
+            highest_bid = Bid.objects.filter(
+                auction=auction, 
+                status=True
+            ).order_by('-amount').first()
 
-                    delivery = Delivery.objects.create(
-                        auction=auction,
-                        user=heighest_bid.bidder,
-                        status=Delivery.DELIVERY_STATUS_PENDING
-                    )
-                
-                # Now return all the bids amount of other user, who have not won the auction
-                # and refund their amount
-                other_bids = Bid.objects.filter(auction=auction).exclude(id=heighest_bid.id)
+            if highest_bid:
+                auction.auction_status = Auction.AUCTION_COMPLETED
+                auction.save()
+
+                Delivery.objects.create(
+                    auction=auction,
+                    customer=highest_bid.bidder,
+                    status=Delivery.DELIVERY_STATUS_PENDING
+                )
+
+                other_bids = Bid.objects.filter(
+                    auction=auction, 
+                    status=True
+                ).exclude(id=highest_bid.id)
 
                 for bid in other_bids:
-                    user_coin = UserCoin.objects.get(user=bid.bidder)
-                    user_coin.amount += bid.amount
+                    user_coin, created = UserCoin.objects.get_or_create(customer=bid.bidder)
+                    user_coin.balance += bid.amount
                     user_coin.save()
                     bid.status = False
                     bid.save()
 
-                    #Save the Transaction
-                    transaction = Transaction.objects.create(
+                    Transaction.objects.create(
                         invoice=f"Refunded for the auction {auction.product.title}",
                         user=bid.bidder,
                         amount=bid.amount,
@@ -48,19 +56,16 @@ def process_completed_auctions():
                         transaction_status=Transaction.TRANSACTION_STATUS_COMPLETED
                     )
 
-                    # Now deduct the amount from the winner and add the amount to the seller
-                    seller = Customer.objects.get(user=auction.product.customer)
-                    seller_coin = UserCoin.objects.get(user=seller.user)
-                    seller_coin.amount += heighest_bid.amount
-                    seller_coin.save()
 
-                    #Save the Transaction
-                    transaction = Transaction.objects.create(
-                        invoice=f"Received {heighest_bid.amount} for the auction {auction.product.title}",
-                        user=seller.user,
-                        amount=heighest_bid.amount,
-                        transaction_type=Transaction.TRANSACTION_TYPE_DEPOSIT,
-                        transaction_status=Transaction.TRANSACTION_STATUS_COMPLETED
-                    )
+                seller = auction.product.customer
+                seller_coin, created = UserCoin.objects.get_or_create(customer=seller.id)
+                seller_coin.balance += highest_bid.amount
+                seller_coin.save()
 
-
+                Transaction.objects.create(
+                    invoice=f"Received {highest_bid.amount} for the auction {auction.product.title}",
+                    user=seller,
+                    amount=highest_bid.amount,
+                    transaction_type=Transaction.TRANSACTION_TYPE_DEPOSITE,
+                    transaction_status=Transaction.TRANSACTION_STATUS_COMPLETED
+                )
